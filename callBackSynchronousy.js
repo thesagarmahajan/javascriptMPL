7ae7a13cf436f2a1fc4746776b56f67dbefff9d1
thesagarmahajan <sagarmahajan31997@gmail.com>
2021-09-01 10:04:55 +0530

    Promises example

---
+++ callBackSynchronousy.js
@@ -0,0 +1,19 @@
+// function validateEmail(e, mycallback){
+//     if(e.length>10 && e.indexOf("@")>=0 && e.indexOf(".")>=0 && e.indexOf("@") < e.indexOf(".")){
+//       mycallback(true);  
+//     }
+//     else{
+//         mycallback(false);
+//     }
+// }
+
+// console.log("START");
+// validateEmail("sagar@gmail.com", (res) => {
+//     res ? console.log("valid email"): console.log("invalid email")
+// });
+// console.log("END");
+
+setTimeout(()=>{
+    console.log("Hi");
+}, 2000);
+console.log("END");
---
+++ promiseExample.js
@@ -0,0 +1,57 @@
+console.log("Start");
+
+// const mypromise = new Promise((resolve, reject)=>{
+//     resolve();
+// });
+
+// mypromise.then(()=>{
+//     var primeNums = [];
+//     for(i=1;i<=50000;i++){
+//         count=0;
+//         for(j=1;j<=i;j++){
+//             if(i%j==0){
+//                 count++;
+//             }
+//         }
+//         if(count==2){
+//             primeNums.push(i);
+//         }
+//     }
+
+//     var selectBox = document.getElementById('selectBoxId');
+//     var dt = '';
+//     primeNums.forEach((value, index) => {
+//         dt += "<option>"+value+"</option>"; 
+//     });
+
+//     selectBox.innerHTML=dt;
+// });
+
+
+const examplePromise = new Promise((resolve, reject)=>{
+    var somevar = [
+        {}
+    ];
+
+    if(somevar.length>0){
+        resolve(somevar);
+    }
+
+    else{
+        reject();
+    }
+    
+});
+
+examplePromise.then((res)=>{
+    console.log("DATA FOUND");
+    console.log(res);
+}).finally(()=>{
+    console.log("EXECUTED SUCCESSFULLY");
+});
+
+examplePromise.catch(()=>{
+    alert("NO Data found! Please try after some time.");
+});
+
+console.log("END");
